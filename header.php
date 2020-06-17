c89ae3d4c30c9c9d2bd2c8aadc0502fb023ecb34
kostasdj <57370817+kostasdj@users.noreply.github.com>
2020-06-17 21:15:50 +0300

    gia thn sofia re gamoto

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
 		<meta charset="utf-8">
 		<meta name="description" content="This is an example of a meta description. This will often show up in search results.">
 		<meta name=viewport content="width=device-width, initial-scale=1">
-		<title>Pik Pok</title>
+		<title>SocialHub</title>
 		<link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" 
             integrity="sha384fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
